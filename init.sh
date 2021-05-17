e4976842fc50dae4a190fd189095f00300ff25e4
MiguelBragaSantos <miguelbragasantos1@gmail.com>
2021-05-17 15:33:06 +0100

    still at it

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-[ -z $VERYFIED_IDENTITY ] && { echo "nonono"; exit 42; }
 
 if [ ! -d ~/.ssh ]; then
 	exec 3>&1;
@@ -14,6 +13,9 @@ if [ ! -d ~/.ssh ]; then
 	ssh-keygen -b 2048 -t rsa -N $PSD -f ~/.ssh/keys -q;
 	chmod 700 ~/.ssh
 	chmod 400 ~/.ssh/keys
+	clear
+	export VERYFIED_IDENTITY=yes
+	./estou_a_ver.sh
 else
 	exec 3>&1;
 	PSD=$(dialog --clear \
@@ -22,6 +24,7 @@ else
 		--passwordbox "Palavra-Passe : " 0 0 2>&1 1>&3);
 	exec 3>&-;
 	ssh-keygen -y -f ~/.ssh/keys -P $PSD
+	clear
 	export VERYFIED_IDENTITY=yes
 	./estou_a_ver.sh
 fi
